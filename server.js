c4c0cb9ccd010aef74a047af3a4b33501f5bdbed
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-05-18 11:21:24 +0530

    heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/CustomerManagementFrontend-Angular'));
+app.use(express.static('./dist/angular-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/CustomerManagementFrontend-Angular/'}),
+    res.sendFile('index.html', {root: 'dist/amgular-frontend/'}),
 );
 
 // Start the app by listening on the default Heroku port
