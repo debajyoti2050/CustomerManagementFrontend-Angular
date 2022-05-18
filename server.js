e39610c02cb54012b3c199c1afed3588399210d5
Debajyoti Bhattacharjee <debajyoti0007@gmail.com>
2022-05-18 11:14:42 +0530

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./distCustomerManagementFrontend-Angular'));
+app.use(express.static('./dist/CustomerManagementFrontend-Angular'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/CustomerManagementFrontend-Angular/'}),
